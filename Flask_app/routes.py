c23560e4fe6e61f4f7d386556bbdc3a0e28bb483
LIOLTU <james@lioltu.com>
2023-02-02 11:05:24 +1000

    Flask_app

---
+++ Flask_app/routes.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Creating a Flask application object
+app = Flask(__name__)
+
+# Defining a route for the root URL '/'
+@app.route('/')
+def index():
+    # Returning the template index.html
+    return render_template('index.html')
+
+# Defining a route for '/result'
+@app.route('/result')
+def result():
+    # Returning the template result.html
+    return render_template('result.html')
