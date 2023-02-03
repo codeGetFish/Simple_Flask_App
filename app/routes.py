faaeecd3ea1c8bd62cc452611c6290c1aa6d8b8a
LIOLTU <james@lioltu.com>
2023-02-03 19:58:30 +1000

    updated file structure

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from app import app
 
 # Creating a Flask application object
 app = Flask(__name__)
@@ -14,3 +15,4 @@ def index():
 def result():
     # Returning the template result.html
     return render_template('result.html')
+
