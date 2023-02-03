4d376758b71435aa5d528960b9abbbf985afb6e3
LIOLTU <james@lioltu.com>
2023-02-03 20:50:41 +1000

    fixed error

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request
 from app import app
 
-# Creating a Flask application object
-app = Flask(__name__)
-
 # Defining a route for the root URL '/'
 @app.route('/')
 def index():
